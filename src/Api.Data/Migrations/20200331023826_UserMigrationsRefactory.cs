ce095e22788c86449e76b5ecc70d17ccba6b704f
Esteban Perea <mondragon1669@gmail.com>
2020-03-30 23:42:49 -0300

    Add Context and Context Factory - Mysql

---
+++ src/Api.Data/Migrations/20200331023826_UserMigrationsRefactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class UserMigrationsRefactory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MyProperty",
+                table: "User");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "_createAt",
+                table: "User",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "_createAt",
+                table: "User");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "MyProperty",
+                table: "User",
+                type: "datetime(6)",
+                nullable: true);
+        }
+    }
+}
---
+++ src/Api.Data/Migrations/20200331024022_UserMigrationsRefactory1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class UserMigrationsRefactory1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "_createAt",
+                table: "User");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreateAt",
+                table: "User",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreateAt",
+                table: "User");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "_createAt",
+                table: "User",
+                type: "datetime(6)",
+                nullable: true);
+        }
+    }
+}
