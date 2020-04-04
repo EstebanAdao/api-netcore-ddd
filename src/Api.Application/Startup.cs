5eece9476c46f247a324f6e44d2c16cc0eb6114d
Esteban Perea <mondragon1669@gmail.com>
2020-04-04 12:41:23 -0300

    Add Swagger

--- src/Api.Application/Startup.cs
+++ src/Api.Application/Startup.cs
@@ -2,9 +2,11 @@ using Api.CrossCutting.DependencyInjection;
 using Api.Data.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Rewrite;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace Application
 {
@@ -22,6 +24,24 @@ namespace Application
         {
             ConfigureService.ConfigureDependenciesService(services);
             ConfigureRepository.ConfigureDependenciesRepository(services);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc(
+                "v1",
+                new OpenApiInfo
+                {
+                    Title = "Curso de AspNetCore 3.1",
+                    Version = "v1",
+                    Description = "Exemplo de API REST criada com ASP.NET Core",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Esteban Perea",
+                        Url = new System.Uri("https://github.com/EstebanAdao")
+                    }
+                });
+            });
+
             services.AddControllers();
         }
 
@@ -32,6 +52,17 @@ namespace Application
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.RoutePrefix = string.Empty;
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
+            var option = new RewriteOptions();
+            option.AddRedirect("^$", "api-docs");
+            app.UseRewriter(option);
 
             app.UseRouting();
 
