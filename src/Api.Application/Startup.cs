6e988429a4d30eb473c96c9077cfd7a95413b732
Esteban Perea <mondragon1669@gmail.com>
2020-04-09 00:17:09 -0300

    Add NewtonSoftJson

--- src/Api.Application/Startup.cs
+++ src/Api.Application/Startup.cs
@@ -17,119 +17,119 @@ using System.Linq;
 
 namespace Application
 {
-	public class Startup
-	{
-		public Startup(IConfiguration configuration)
-		{
-			Configuration = configuration;
-		}
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
 
-		public IConfiguration Configuration { get; }
+        public IConfiguration Configuration { get; }
 
-		// This method gets called by the runtime. Use this method to add services to the container.
-		public void ConfigureServices(IServiceCollection services)
-		{
-			ConfigureService.ConfigureDependenciesService(services);
-			ConfigureRepository.ConfigureDependenciesRepository(services);
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            ConfigureService.ConfigureDependenciesService(services);
+            ConfigureRepository.ConfigureDependenciesRepository(services);
 
-			var signingConfigurations = new SigningConfigurations();
-			services.AddSingleton(signingConfigurations);
+            var signingConfigurations = new SigningConfigurations();
+            services.AddSingleton(signingConfigurations);
 
-			var tokenConfigurations = new TokenConfigurations();
-			new ConfigureFromConfigurationOptions<TokenConfigurations>(
-				Configuration.GetSection("TokenConfigurations"))
-				.Configure(tokenConfigurations);
-			services.AddSingleton(tokenConfigurations);
+            var tokenConfigurations = new TokenConfigurations();
+            new ConfigureFromConfigurationOptions<TokenConfigurations>(
+                Configuration.GetSection("TokenConfigurations"))
+                .Configure(tokenConfigurations);
+            services.AddSingleton(tokenConfigurations);
 
-			services.AddAuthentication(authOptions =>
-			{
-				authOptions.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-				authOptions.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            services.AddAuthentication(authOptions =>
+            {
+                authOptions.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                authOptions.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
 
-			}).AddJwtBearer(bearerOptions =>
-			{
-				var paramsValidation = bearerOptions.TokenValidationParameters;
-				paramsValidation.IssuerSigningKey = signingConfigurations.Key;
-				paramsValidation.ValidAudience = tokenConfigurations.Audience;
-				paramsValidation.ValidIssuer = tokenConfigurations.Issuer;
-				paramsValidation.ValidateIssuerSigningKey = true;
-				paramsValidation.ValidateLifetime = true;
-				paramsValidation.ClockSkew = TimeSpan.Zero;
-			});
+            }).AddJwtBearer(bearerOptions =>
+            {
+                var paramsValidation = bearerOptions.TokenValidationParameters;
+                paramsValidation.IssuerSigningKey = signingConfigurations.Key;
+                paramsValidation.ValidAudience = tokenConfigurations.Audience;
+                paramsValidation.ValidIssuer = tokenConfigurations.Issuer;
+                paramsValidation.ValidateIssuerSigningKey = true;
+                paramsValidation.ValidateLifetime = true;
+                paramsValidation.ClockSkew = TimeSpan.Zero;
+            });
 
-			services.AddAuthorization(auth =>
-			{
-				auth.AddPolicy("Bearer", new AuthorizationPolicyBuilder()
-					.AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
-					.RequireAuthenticatedUser().Build());
-			});
+            services.AddAuthorization(auth =>
+            {
+                auth.AddPolicy("Bearer", new AuthorizationPolicyBuilder()
+                    .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+                    .RequireAuthenticatedUser().Build());
+            });
 
-			services.AddSwaggerGen(c =>
-			{
-				c.SwaggerDoc(
-				"v1",
-				new OpenApiInfo
-				{
-					Title = "Curso de AspNetCore 3.1",
-					Version = "v1",
-					Description = "Exemplo de API REST criada com ASP.NET Core",
-					Contact = new OpenApiContact
-					{
-						Name = "Esteban Perea",
-						Url = new Uri("https://github.com/EstebanAdao")
-					}
-				});
-				c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-				{
-					In = ParameterLocation.Header,
-					Description = "Entre com o token JWT",
-					Name = "Authorization",
-					Type = SecuritySchemeType.ApiKey
-				});
-				c.AddSecurityRequirement(new OpenApiSecurityRequirement {
-					{
-						new OpenApiSecurityScheme
-						{
-							Reference = new OpenApiReference{
-								Type = ReferenceType.SecurityScheme,
-								Id = "Bearer"
-							}
-						},
-						new List<string>()
-					}
-				});
-			});
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc(
+                "v1",
+                new OpenApiInfo
+                {
+                    Title = "Curso de AspNetCore 3.1",
+                    Version = "v1",
+                    Description = "Exemplo de API REST criada com ASP.NET Core",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Esteban Perea",
+                        Url = new Uri("https://github.com/EstebanAdao")
+                    }
+                });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Entre com o token JWT",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference{
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new List<string>()
+                    }
+                });
+            });
 
-			services.AddControllers();
-		}
+            services.AddControllers().AddNewtonsoftJson();
+        }
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-		{
-			if (env.IsDevelopment())
-			{
-				app.UseDeveloperExceptionPage();
-			}
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
 
-			app.UseSwagger();
-			app.UseSwaggerUI(c =>
-			{
-				c.RoutePrefix = string.Empty;
-				c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-			});
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.RoutePrefix = string.Empty;
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
-			var option = new RewriteOptions();
-			option.AddRedirect("^$", "api-docs");
-			app.UseRewriter(option);
+            var option = new RewriteOptions();
+            option.AddRedirect("^$", "api-docs");
+            app.UseRewriter(option);
 
-			app.UseRouting();
+            app.UseRouting();
 
-			app.UseAuthorization();
+            app.UseAuthorization();
 
-			app.UseEndpoints(endpoints =>
-			{
-				endpoints.MapControllers();
-			});
-		}
-	}
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
 }
